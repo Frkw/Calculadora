c8134b822b72771d0b3ee93777963c39fd678360
wilson <willl_agu@hotmail.com>
2016-07-02 09:07:14 -0500

    definicion variables de prueba

--- app/src/main/java/com/example/andre/calculadora/MainActivity.java
+++ app/src/main/java/com/example/andre/calculadora/MainActivity.java
@@ -12,8 +12,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //declarar variables para botones
         Button  uno = (Button) findViewById(R.id.one);
         Button  dos = (Button) findViewById(R.id.two);
+        Button  tres =(Button) findViewById(R.id.three);
+        Button  cuatro = (Button) findViewById(R.id.four);
+        Button  cinco = (Button) findViewById(R.id.five);
+        Button  seis = (Button) findViewById(R.id.six);
+        Button  siete = (Button) findViewById(R.id.seven);
+        Button  ocho = (Button) findViewById(R.id.eight);
+        Button  nueve = (Button) findViewById(R.id.nine);
+        Button  cero = (Button) findViewById(R.id.zero);
+        //asociar evento click en cad uno de los botones
 
     }
 
